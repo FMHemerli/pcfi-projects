e6051ac6795c4a6e98d6647dfc8c19100353ef1b
Flávio Manoel S Hemerli <fm.hemerli@gmail.com>
2021-08-15 16:38:27 -0300

    Fix improper naming

--- miscellaneous-tools/translate-input-contract.py
+++ miscellaneous-tools/translate-input-contract.py
@@ -4,23 +4,23 @@ from datetime import datetime as dt2, timedelta as td
 with open("translate-input-contract.json") as data:
     file = json.loads(data.read())
     for contract in file:
-        contract['contrato']['clientepessoafisica']['cpf'] = contract['contrato']['clientepessoafisica']['cpf'].replace(".", "").replace("-", "")
-        contract['contrato']['datacontrato'] = contract['contrato']['datacontrato'].replace(" 00:00:00", "")
-        contract['contrato']['dataprimeirovencto'] = contract['contrato']['dataprimeirovencto'].replace(" 00:00:00", "")
-        contract['contrato']['dataultimovencto'] = contract['contrato']['dataultimovencto'].replace(" 00:00:00", "")
-        contract['contrato']['clientepessoafisica']['rg']['dataexpedicao'] = contract['contrato']['clientepessoafisica']['rg']['dataexpedicao'].replace(" 00:00:00", "")
-        contract['operacao']['dataoperacao'] = contract['operacao']['dataoperacao'].replace(" 00:00:00", "")
-        contract['contrato']['clientepessoafisica']['residencia']['endereco']['cep']=contract['contrato']['clientepessoafisica']['residencia']['endereco']['cep'].replace(".", "").replace("-", "")
-        contract['contrato']['diapadraovencto'] = contract['contrato']['diapadraovencto'].replace(" 00:00:00", "")
-        contract['contrato']['diapadraovencto'] = dt2.strftime(dt2.strptime(contract['contrato']['dataprimeirovencto'], "%Y-%m-%d"), "%d")
-        contract['contrato']['plano'] = "9018"
-        contract['contrato']['valorfinanciado'] = str(float(contract['contrato']['valorsolicitado']) + float(
-            contract['contrato']['iof']))
-        contract['contrato']['diascarencia'] = str((dt2.strptime(
-            contract['contrato']['dataprimeirovencto'].replace(" 00:00:00", ""),
+        contract['cont']['clientpf']['cpf'] = contract['cont']['clientpf']['cpf'].replace(".", "").replace("-", "")
+        contract['cont']['dtcont'] = contract['cont']['dtcont'].replace(" 00:00:00", "")
+        contract['cont']['dtfirstdue'] = contract['cont']['dtfirstdue'].replace(" 00:00:00", "")
+        contract['cont']['dtlastdue'] = contract['cont']['dtlastdue'].replace(" 00:00:00", "")
+        contract['cont']['clientpf']['idn']['dtexp'] = contract['cont']['clientpf']['idn']['dtexp'].replace(" 00:00:00", "")
+        contract['op']['dtop'] = contract['op']['dtop'].replace(" 00:00:00", "")
+        contract['cont']['clientpf']['resid']['adr']['cep']=contract['cont']['clientpf']['resid']['adr']['cep'].replace(".", "").replace("-", "")
+        contract['cont']['daystddue'] = contract['cont']['daystddue'].replace(" 00:00:00", "")
+        contract['cont']['daystddue'] = dt2.strftime(dt2.strptime(contract['cont']['dtfirstdue'], "%Y-%m-%d"), "%d")
+        contract['cont']['plan'] = ""
+        contract['cont']['amntfincd'] = str(float(contract['cont']['amntsolicit']) + float(
+            contract['cont']['iof']))
+        contract['cont']['dayscarencia'] = str((dt2.strptime(
+            contract['cont']['dtfirstdue'].replace(" 00:00:00", ""),
             "%Y-%m-%d"
         ) - dt2.strptime(
-            contract['operacao']['dataoperacao'].replace(" 00:00:00", ""),
+            contract['op']['dtop'].replace(" 00:00:00", ""),
             "%Y-%m-%d"
         ) - td(days=30)).days)
 data.close()
